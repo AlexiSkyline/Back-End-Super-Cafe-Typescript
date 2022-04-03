d143a15782eef8ac31ba0a51334fe58b33b90b8b
AlexiSkyline <ilegal_sprite@outlook.com>
2022-04-02 21:19:00 -0600

    Agregamos el Schema de los Usuarios

---
+++ interfaces/Usuario.ts
@@ -0,0 +1,11 @@
+interface Usuario {
+    nombre: string;
+    correo: string;
+    password: string;
+    img: string;
+    rol: string;
+    estado: boolean;
+    google: boolean;
+}
+
+export default Usuario;
