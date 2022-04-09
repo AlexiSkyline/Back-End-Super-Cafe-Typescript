5407445cb1a562e5ffe5c4d667048f067e001626
AlexiSkyline <ilegal_sprite@outlook.com>
2022-04-08 22:01:46 -0500

    Agregamos el metodo para buscar si el email ya esta usado en DBValidators

--- src/helpes/DB-Validators.ts
+++ src/helpes/DB-Validators.ts
@@ -19,6 +19,18 @@ class DBValidator {
         }
     }
 
+    /*
+        * Validate if the email is already registered in the database
+        * @method: isValidEmail
+        * @params email: string - email to validate and find in the database
+    */
+    public static async findEmail( email: string ): Promise<void> {
+        // Todo: check if the email exists
+        const existEmail = await UserSchema.findOne({ email });
+        if( existEmail ) {
+            throw new Error( `The email '${ email }' is already registered` );
+        }
+    }
 }
 
 export default DBValidator;
