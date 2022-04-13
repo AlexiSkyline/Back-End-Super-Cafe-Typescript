6dae892d0ceab9120fafb9215964fc0f8bb81817
AlexiSkyline <ilegal_sprite@outlook.com>
2022-04-12 21:57:02 -0500

    Agregamos el metodo googleSignIn en la clase Auth

--- src/Controller/Auth.ts
+++ src/Controller/Auth.ts
@@ -3,6 +3,7 @@ import bcryptjs from 'bcryptjs';
 
 import { UserSchema } from '../Models/Index';
 import GeneratorJWT from '../Helpers/GeneratorJWT';
+import GoogleActions from '../Helpers/GoogleActions';
 
 class Auth {
     public static async Login( req: Request, res: Response ): Promise<Response> {
@@ -45,6 +46,45 @@ class Auth {
             });
         } catch (error) {
             return res.status(500).json({ ok: false, message: 'Failed to login' });
+        }
+    }
+
+    public static async googleSignIn( req: Request, res: Response ): Promise<Response> {
+        const { id_token } = req.body;
+
+        try {
+            // * Check if the email exists
+            const { email, name, img }: any = await GoogleActions.googleVerify( id_token );
+            let user = await UserSchema.findOne({ email });
+
+            // * If the user does not exist
+            if( !user ) {
+                const data: any = {
+                    name,
+                    email,
+                    password: 'password_for_google',
+                    img,
+                    google: true
+                }
+
+                user = new UserSchema( data );
+                await user.save();
+            }
+
+            // * If the user in the DB is inactive
+            if( !user.status ) {
+                return res.status( 401 ).json({
+                    ok: false,
+                    message: 'Talk to administrator, user blocked' 
+                });
+            }
+
+            // * Generate the Token
+            const token = await GeneratorJWT.generateToken( user.id );
+
+            return res.status(201).json({ ok: true, user, token });
+        } catch (error) {
+            return res.status(400).json({ ok: false, message: 'Token of Google is not Valid' });
         }
     }
 }
--- src/Helpers/GoogleActions.ts
+++ src/Helpers/GoogleActions.ts
@@ -2,7 +2,7 @@ import { OAuth2Client } from 'google-auth-library';
 
 class GoogleActions {
     private static client: OAuth2Client;
-    public static async googleVerify( idToken: string ) {
+    public static async googleVerify( idToken: string ): Promise<Object> {
         this.client = new OAuth2Client( process.env.GOOGLE_CLIENT_ID );
 
         const ticket = await this.client.verifyIdToken({ 
