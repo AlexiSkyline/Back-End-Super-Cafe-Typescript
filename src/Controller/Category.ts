cfabb3233751f7237adf2ebe9612db5640be5570
AlexiSkyline <ilegal_sprite@outlook.com>
2022-04-13 20:37:17 -0500

    Agreamos el metodo createCategory en Category

--- src/Controller/Category.ts
+++ src/Controller/Category.ts
@@ -10,11 +10,11 @@ class Category {
         try {
             const [ total, categories ] = await Promise.all([
                 CategorySchema.countDocuments( query ),
-                CategorySchema.find( query ).populate( 'usuer', 'name' )
+                CategorySchema.find( query ).populate( 'user', 'name' )
                     .skip( Number( since) ).limit( Number(limit) )
             ]);
             
-            return res.status( 200 ).json({
+            return res.status(200).json({
                 ok: true,
                 total,
                 categories
@@ -31,21 +31,46 @@ class Category {
         const { id } = req.params;
 
         try {
-            const category: ICategory | null = await CategorySchema.findById( id ).populate( 'usuer', 'name' );
+            const category: ICategory | null = await CategorySchema.findById( id ).populate( 'user', 'name' );
             
             if( !(category!.status) ) {
-                return res.status( 400 ).json({
+                return res.status(400).json({
                     ok: false,
                     message: 'Category is not active',
                 });
             }
 
-            return res.status( 200 ).json({ ok: true, category });
+            return res.status(200).json({ ok: true, category });
         } catch (error) {
             return res.status(500).json({
                 ok: false,
                 message: 'Error getting category',
             });
+        }
+    }
+
+    public static async createCategory( req: any, res: Response ): Promise<Response> {
+        const name = req.body.name.toUpperCase();
+        
+        try {
+            const categoryDB = await CategorySchema.findOne({ name });
+            if( categoryDB ) { 
+                return res.status(400).json({
+                    ok: false,
+                    message: `Category: ${ name } already exists`
+                });
+            }
+
+            const data = {
+                name,
+                user: req.user._id
+            }
+            const newCategory = new CategorySchema( data );
+            await newCategory.save();
+
+            return res.status(201).json({ ok: true, category: newCategory });
+        } catch (error) {
+            return req.status(500).json({ ok: false, message: 'Error creating category' });
         }
     }
 }
