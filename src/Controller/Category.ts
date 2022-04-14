fa4cbc8e2e381f4b540ce751b23c8f68554f0cb3
AlexiSkyline <ilegal_sprite@outlook.com>
2022-04-13 20:31:15 -0500

    Agregamos el metodo getCategoryById en Category

--- src/Controller/Category.ts
+++ src/Controller/Category.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { CategorySchema } from '../Models/Index';
+import ICategory from '../Interfaces/Category';
 
 class Category {
     public static async getCategories( req: Request, res: Response ): Promise<Response> {
@@ -9,7 +10,8 @@ class Category {
         try {
             const [ total, categories ] = await Promise.all([
                 CategorySchema.countDocuments( query ),
-                CategorySchema.find( query ).skip( Number( since) ).limit( Number(limit) )
+                CategorySchema.find( query ).populate( 'usuer', 'name' )
+                    .skip( Number( since) ).limit( Number(limit) )
             ]);
             
             return res.status( 200 ).json({
@@ -22,6 +24,28 @@ class Category {
                 ok: false,
                 message: 'Error getting categories',
             });   
+        }
+    }
+
+    public static async getCategoryById( req: Request, res: Response ): Promise<Response> {
+        const { id } = req.params;
+
+        try {
+            const category: ICategory | null = await CategorySchema.findById( id ).populate( 'usuer', 'name' );
+            
+            if( !(category!.status) ) {
+                return res.status( 400 ).json({
+                    ok: false,
+                    message: 'Category is not active',
+                });
+            }
+
+            return res.status( 200 ).json({ ok: true, category });
+        } catch (error) {
+            return res.status(500).json({
+                ok: false,
+                message: 'Error getting category',
+            });
         }
     }
 }
