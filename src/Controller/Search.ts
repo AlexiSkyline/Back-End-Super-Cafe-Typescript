2c1e86f800047cf566d78040a63fc9863c4c88b5
AlexiSkyline <ilegal_sprite@outlook.com>
2022-04-14 23:54:30 -0500

    Movimos el metodo selectsSearch y Agregamos la ruta buscar

--- src/Controller/Search.ts
+++ src/Controller/Search.ts
@@ -3,37 +3,7 @@ import { Types } from 'mongoose';
 import { CategorySchema, ProductSchema, UserSchema } from '../Models/Index';
 
 class Search {
-    private allowedCollections: string[] = [ 'users', 'category', 'products', 'roles' ];
-
-    public selectsSearch( req: Request, res: Response ): Response | void {
-        const { collection, term } = req.params;
-
-        if( !this.allowedCollections.includes( collection ) ) {
-            return res.status(400).json({
-                ok: false,
-                message: `The allowed collections are: ${ this.allowedCollections }`
-            });
-        }
-
-        switch( collection ) {
-            case 'user':
-                this.searchUser( term, res );
-                break;
-            case 'category':
-                this.searchCategory( term, res );
-                break;
-            case 'products':
-                this.searchProduct( term, res );
-                break;
-            default: 
-                return res.status(500).json({ 
-                    ok: false, 
-                    message: `We couldn't search the collection: ${ term }`
-                });
-        }
-    }
-
-    private async searchUser( term: string, res: Response ): Promise<Response> {
+    public async searchUser( term: string, res: Response ): Promise<Response> {
         const isMongoID: boolean = Types.ObjectId.isValid( term );
 
         if( isMongoID ) {
@@ -53,7 +23,7 @@ class Search {
         return res.status(200).json({ ok: true, resuts: users });
     }
 
-    private async searchCategory( term: string, res: Response ): Promise<Response> {
+    public async searchCategory( term: string, res: Response ): Promise<Response> {
         const isMongoID: boolean = Types.ObjectId.isValid( term );
         if( isMongoID ) {
             const category = await CategorySchema.findById( term );
@@ -69,7 +39,7 @@ class Search {
         return res.status(200).json({ ok: true, resuts: categories });
     }
 
-    private async searchProduct( term: string, res: Response ): Promise<Response> {
+    public async searchProduct( term: string, res: Response ): Promise<Response> {
         const isMongoId: boolean = Types.ObjectId.isValid( term );
         if( isMongoId ) {
             const product = await ProductSchema.findById( term );
@@ -85,3 +55,6 @@ class Search {
         return res.status(200).json({ ok: true, results: products });
     }
 }
+
+const search = new Search();
+export default search;
--- src/Routes/SearchRoutes.ts
+++ src/Routes/SearchRoutes.ts
@@ -1,4 +1,5 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
+import SearchController from '../Controller/Search';
 
 class SearchRouter {
     public router: Router;
@@ -7,13 +8,41 @@ class SearchRouter {
         this.routes();
     }
 
+    public selectsSearch( req: Request, res: Response ): Response | void {
+        const allowedCollections: string[] = ['users', 'categories', 'products', 'roles'] ;
+        const { collection, term } = req.params;
+    
+        if( !allowedCollections.includes( collection ) ) {
+            return res.status(400).json({
+                ok: false,
+                message: `The allowed collections are: ${ allowedCollections }`
+            });
+        }
+    
+        switch( collection ) {
+            case 'users':
+                SearchController.searchUser( term, res );
+                break;
+            case 'categories':
+                SearchController.searchCategory( term, res );
+                break;
+            case 'products':
+                SearchController.searchProduct( term, res );
+                break;
+            default: 
+                return res.status(500).json({ 
+                    ok: false, 
+                    message: `We couldn't search the collection: ${ term }`
+                });
+        }
+    }
     /*
         *
         * /api/buscar/:colección/:termino
         *    
     */
     routes() {
-        this.router.get( '/:collection/:term' );
+        this.router.get( '/:collection/:term', this.selectsSearch );
     }
 }
 
